f0ebebc56e0bdcfc201114f82c0db97f820e4281
mofm <emre.eryilmaz@piesso.com>
2020-09-05 16:56:08 +0300

    changed script for gstatus-1.0.x versions

--- gstatus_discovery.py
+++ gstatus_discovery.py
@@ -5,22 +5,23 @@ import sys
 
 gluster_volume_names = []
 gstatus_output = subprocess.check_output('gstatus -a -o json ', shell=True).decode()
-date, time, json_part = gstatus_output.split(maxsplit=2)
-gluster_info = json.loads(json_part)
-volume_list = gluster_info["volume_summary"]
+gluster_info = json.loads(gstatus_output)
+volume_list = gluster_info["data"]["volume_summary"]
+
 
 nargs = len(sys.argv)
 
 if nargs == 1:
     for volume in volume_list:
-        gluster_volume_names.append({"{#VOLUME_NAME}": volume["volume_name"]})
+        gluster_volume_names.append({"{#VOLUME_NAME}": volume["name"]})
     print(json.dumps({'data': gluster_volume_names}))
 
 elif nargs == 2:
-    print(gluster_info[sys.argv[1]])
+    print(gluster_info["data"][sys.argv[1]])
+
 elif nargs == 3:
     for volume in volume_list:
-        if volume.get('volume_name') and sys.argv[2] == volume["volume_name"]:
+        if volume.get('name') and sys.argv[2] == volume["name"]:
             print(volume[sys.argv[1]])
             break
     else:
@@ -28,7 +29,7 @@ elif nargs == 3:
             print('down')
         else:
             print()
-        
+
 else:
     print('Wrong arguments')
 
